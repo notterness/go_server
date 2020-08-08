690319a3e6472869bcb92c5ee1976ceb14ccfddf
Noel Otterness <notterness@gmail.com>
2020-08-08 12:55:54 -0600

    Added the time component to the statistics. Fixed a bunch of warnings in the code.

--- hashMethodHandler.go
+++ hashMethodHandler.go
@@ -6,14 +6,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 var mu sync.Mutex
-var count int = 1
+var count = 0
 
+/*
+** The requiredFormFields array of String is used to validate form data that is passed into the "POST /hash"
+**   method. Currently, there is only one required form field, but to add more, simply update the
+**   RequiredFormFields constant and add the values in the initializeHash() function.
+ */
 const RequiredFormFields = 1
 const PasswordFormField = "password"
 
@@ -26,7 +33,13 @@ func initializeHash() {
 	requiredFormFields[0] = PasswordFormField
 }
 
+/*
+** This is the handler for the "PUT /hash" method. If there is not an error in the parsing of either the
+**   method fields or the form data, it will return the number of times this has been called (inclusive of ths call).
+ */
 func hash(w http.ResponseWriter, r *http.Request) {
+
+	defer measurePostTime(time.Now().UnixNano())
 
 	/*
 	** Duplicate code, but rather than passing in a different parameter (and making the method handler maps way more
@@ -43,7 +56,7 @@ func hash(w http.ResponseWriter, r *http.Request) {
 	** Parse out the form fields and make sure that "password" is present
 	 */
 	if err := r.ParseForm(); err != nil {
-		log.Print(err)
+		_, _ = fmt.Fprintf(os.Stderr, "hashWithQualifier() ParseForm: %v\n", err)
 	}
 	for k, v := range r.Form {
 		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
@@ -53,11 +66,14 @@ func hash(w http.ResponseWriter, r *http.Request) {
 		numOfStr := len(methodStrings)
 		if numOfStr == 2 {
 			mu.Lock()
-			tmp := count
 			count++
+			tmp := count
 			mu.Unlock()
 
-			fmt.Fprintf(w, "Count %d\n", tmp)
+			n, err := fmt.Fprintf(w, "%d\n", tmp)
+			if err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "hash(1) Fprintf: %d %v\n", n, err)
+			}
 		} else {
 			/*
 			** UNPROCESSABLE_ENTITY_422
@@ -65,7 +81,10 @@ func hash(w http.ResponseWriter, r *http.Request) {
 			** Since the number of qualifiers was not 0, return UNPROCESSABLE_ENTITY since the code should not
 			**   return anything unexpected method qualifiers.
 			 */
-			fmt.Fprintf(w, "{\"error\": 422}\n")
+			n, err := fmt.Fprintf(w, "{\"error\": 422}\n")
+			if err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "hash(2) Fprintf: %d %v\n", n, err)
+			}
 		}
 	} else {
 		/*
@@ -73,7 +92,10 @@ func hash(w http.ResponseWriter, r *http.Request) {
 		**
 		** If all of the required form fields are not present, return the PRECONDITION_FAILED error code
 		 */
-		fmt.Fprintf(w, "{\"error\": 412}\n")
+		n, err := fmt.Fprintf(w, "{\"error\": 412}\n")
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "hash(3) Fprintf: %d %v\n", n, err)
+		}
 	}
 }
 
@@ -108,25 +130,34 @@ func hashWithQualifier(w http.ResponseWriter, r *http.Request) {
 			** Since the value passed in was not an integer, return UNPROCESSABLE_ENTITY since the code should not
 			**   return anything for a garbage method qualifier.
 			 */
-			fmt.Fprintf(w, "{\"error\": 422}\n")
+			n, err := fmt.Fprintf(w, "{\"error\": 422}\n")
+			if err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "hashWithQualifier(1) Fprintf: %d %v\n", n, err)
+			}
 		}
 	} else {
 		/*
 		** UNPROCESSABLE_ENTITY_422
 		**
-		** Since the number of qualifiers was not 0, return UNPROCESSABLE_ENTITY since the code should not
+		** Since the number of qualifiers was not 1, return UNPROCESSABLE_ENTITY since the code should not
 		**   return anything unexpected method qualifiers.
 		 */
-		fmt.Fprintf(w, "{\"error\": 422}\n")
+		n, err := fmt.Fprintf(w, "{\"error\": 422}\n")
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "hashWithQualifier(2) Fprintf: %d %v\n", n, err)
+		}
 	}
 }
 
 func performHash(qualifier int64, password string) {
 	h := sha512.New()
 	h.Write([]byte(password))
-	base64ResultStr := string(base64.StdEncoding.EncodeToString(h.Sum(nil)))
+	base64ResultStr := base64.StdEncoding.EncodeToString(h.Sum(nil))
 
-	fmt.Printf("%d base64: %s", qualifier, base64ResultStr)
+	n, err := fmt.Printf("%d base64: %s", qualifier, base64ResultStr)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Fprintf: %d %v\n", n, err)
+	}
 }
 
 /*
@@ -134,7 +165,7 @@ func performHash(qualifier int64, password string) {
 **   required form fields are present.
  */
 func validateFormData(r *http.Request) bool {
-	var success bool = true
+	var success = true
 
 	for i := 0; i < RequiredFormFields; i++ {
 		result := r.FormValue(requiredFormFields[i])
@@ -144,4 +175,21 @@ func validateFormData(r *http.Request) bool {
 	}
 
 	return success
+}
+
+/*
+** The following is a deferred function used to compute the time required to handle the POST functions.
+**
+** NOTE: This is not a perfect representation of the processing time for the POST /hash handler as there is
+**   work that was done prior to this call to determine the request type (and the work in the HTTP server just to
+**   receive the request). But, it does give a measure of the specific handler so that can be evaluated for
+**   improvements.
+ */
+func measurePostTime(start int64) {
+	elapsed := (time.Now().UnixNano() - start) / int64(time.Microsecond)
+	log.Printf("POST /hash took %d", elapsed)
+
+	mu.Lock()
+	totalTime += elapsed
+	mu.Unlock()
 }
